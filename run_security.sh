c9352e42a61b62ba95388177917ed0aadfe7ef4e
yjj3019 <magicsoma@hanmail.net>
2025-10-02 14:17:51 +0900

    Update run_security.sh

--- run_security.sh
+++ run_security.sh
@@ -15,7 +15,7 @@ function send(){
 }
 
 # no proxy
-export no_proxy="localhost, 127.0.0.1, 172.x.x.113"
+export no_proxy="localhost, 127.0.0.1, 172.21.135.113"
 
 
 # --- 스크립트 설명 ---
@@ -24,10 +24,13 @@ export no_proxy="localhost, 127.0.0.1, 172.x.x.113"
 # 2. requirements.txt에 명시된 필수 패키지를 설치합니다.
 # 3. gunicorn WSGI 서버를 사용하여 Flask 애플리케이션을 실행합니다.
 
-export LLM_URL="http://172.x.x.113:4000" # 여기에 LLM 서버 URL을 입력하세요. 예: http://192.168.1.10:8080
-export MODEL="S-Core/Qwen3-235B-A22B"   # 여기에 사용할 모델 이름을 입력하세요. 예: mistral-7b-instruct-v0.2.Q4_K_M.gguf
+#export LLM_URL="http://172.21.135.113:4000" # 여기에 LLM 서버 URL을 입력하세요. 예: http://192.168.1.10:8080
+#export MODEL="S-Core/Qwen3-235B-A22B"   # 여기에 사용할 모델 이름을 입력하세요. 예: mistral-7b-instruct-v0.2.Q4_K_M.gguf
 #export MODEL="openai/gpt-oss-120b"   # 여기에 사용할 모델 이름을 입력하세요. 예: mistral-7b-instruct-v0.2.Q4_K_M.gguf
-export TOKEN="sk-xxxxxx"   # LLM 서버에 토큰이 필요하면 여기에 입력하세요. (선택 사항)
+#export TOKEN="sk-iX9Azu30JV3E-WByjwm2PQ"   # LLM 서버에 토큰이 필요하면 여기에 입력하세요. (선택 사항)
+export PROXY="http://30.30.30.27:8080"
+export SERVER="http://172.21.18.235:5000"
+#export NOPROXY="172.21.135.113"
 
 # waitress 서버 실행 (기존 app.py와 호환)
-/usr/bin/python3.6  /data/iso/AIBox/security.py --llm-url "$LLM_URL" --model "$MODEL" --token "$TOKEN" | send
+/usr/bin/python3.6  /data/iso/AIBox/security.py --server-url "$SERVER" --proxy "$PROXY"| send
